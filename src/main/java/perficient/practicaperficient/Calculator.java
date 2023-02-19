cdd86c9adbf303d2caa915f91f558c0ffc6548fd
DArt38 <danielararat@unicomfacauca.edu.co>
2023-02-18 21:15:14 -0500

    Creacion clase calculator con metodos multiplicacion y division

---
+++ src/main/java/perficient/practicaperficient/Calculator.java
@@ -0,0 +1,12 @@
+package perficient.practicaperficient;
+
+public class Calculator {
+
+    public int multiplicacion(int a, int b){
+        return a * b ;
+    }
+
+    public int division(int a, int b){
+        return a / b;
+    }
+}
